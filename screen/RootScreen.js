db05884b226dc195430d6c4a7680bba62572c633
Diego Arturo Hernandez Fuentes <diegohdez12@gmail.com>
2023-12-05 13:54:30 -0600

    feat: add root screen

--- screen/RootScreen.js
+++ screen/RootScreen.js
@@ -1,10 +1,15 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import PrimaryButton from '../components/PrimaryButton'
+import { TextInput } from 'react-native'
 
 export default function RootScreen() {
   return (
     <View>
-      <Text>RootScreen</Text>
+      <Text>Welcome</Text>
+      <TextInput />
+      <PrimaryButton>Reset</PrimaryButton>
+      <PrimaryButton>Confirm</PrimaryButton>
     </View>
   )
 }
