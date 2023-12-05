207d1975ddf900cb6d53af9f4b2199c68f009bfd
Diego Arturo Hernandez Fuentes <diegohdez12@gmail.com>
2023-12-05 14:37:02 -0600

    feat: add root screen

--- screen/RootScreen.js
+++ screen/RootScreen.js
@@ -1,11 +1,11 @@
-import { View, Text } from 'react-native'
+import { View, StyleSheet, Text } from 'react-native'
 import React from 'react'
 import PrimaryButton from '../components/PrimaryButton'
 import { TextInput } from 'react-native'
 
 export default function RootScreen() {
   return (
-    <View>
+    <View style={styles.inputContainer}>
       <Text>Welcome</Text>
       <TextInput />
       <PrimaryButton>Reset</PrimaryButton>
@@ -13,3 +13,22 @@ export default function RootScreen() {
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    padding: 16,
+    alignItems: 'center',
+    justifyContent: 'center',
+    alignContent: 'center',
+    backgroundColor: '#be0202',
+    borderRadius: 12,
+    elevation: 8,
+    shadowColor: 'black',
+    shadowOffset: {
+      width: 0,
+      height: 2
+    },
+    shadowRadius: 6,
+    shadowOpacity: 0.25,
+  }
+})
