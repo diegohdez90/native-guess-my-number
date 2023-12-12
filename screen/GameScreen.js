5108dbf7da423c800a4f52371adf8987d47f73a9
Diego Arturo Hernandez Fuentes <diegohdez12@gmail.com>
2023-12-12 13:52:19 -0600

    feat: add button to guess if it is lower or greater

--- screen/GameScreen.js
+++ screen/GameScreen.js
@@ -1,20 +1,49 @@
-import { View, Text, StyleSheet } from 'react-native'
+import { Alert, View, Text, StyleSheet } from 'react-native'
 import React, { useState } from 'react'
 import Title from '../components/ui/Title'
 import { generateRandom } from '../utils/functions'
 import NumberCard from '../components/game/NumberCard';
+import PrimaryButton from '../components/ui/PrimaryButton';
+
+let minBoundary = 1;
+let maxBoundary = 100;
 
 export default function GameScreen({
   userNumber
 }) {
-  const initialGuess = generateRandom(1, 100, userNumber);
-  const [currentGuess, setCurrentGuess] = useState(userNumber);
+  const initialGuess = generateRandom(minBoundary, maxBoundary, userNumber);
+  const [currentGuess, setCurrentGuess] = useState(initialGuess);
+
+  const onNextGuessHandler = (direction) => {
+    if (
+      (direction === 'lower' && currentGuess < userNumber) ||
+      (direction === 'greater' && currentGuess > userNumber)
+    ) {
+      Alert.alert('Don\'t lie', 'You know that this is wrong...', [{
+        text: 'Sorry!',
+        style: 'cancel'
+      }]);
+      return;
+    }
+    if (direction === 'lower') {
+      maxBoundary = currentGuess ;
+    } else {
+      minBoundary = currentGuess + 1;
+    }
+    const newRandom = generateRandom(minBoundary, maxBoundary, currentGuess);
+    setCurrentGuess(newRandom);
+  }
+
   return (
     <View style={styles.container}>
       <Title>You guess the number</Title>
       <NumberCard>{currentGuess}</NumberCard>
       <View>
         <Text>Higher or lower</Text>
+        <View>
+          <PrimaryButton onPress={onNextGuessHandler.bind(this, 'lower')}>-</PrimaryButton>
+          <PrimaryButton onPress={onNextGuessHandler.bind(this, 'greater')}>+</PrimaryButton>
+        </View>
       </View>
     </View>
   )
